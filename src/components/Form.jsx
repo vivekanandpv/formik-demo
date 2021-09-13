5cba5c940da540ae2ce00708b3424c359a3e6e4d
vivekanandpv <vivekanandpv@outlook.com>
2021-09-13 10:38:42 +0530

    done

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -3,8 +3,10 @@ import React from 'react';
 import { Fragment } from 'react/cjs/react.production.min';
 
 const Form = (props) => {
-  //      formikInstance.errors is the error state of the form
-  //      use it to conditionally render the field errors
+  //      to keep track of which input fields are touched in the form
+  //      we use formikInstace.touched.<property>
+  //      In order for this to work, every control's onBlur should be assigned to formikInstace.handleBlur
+  //      Use this to display the validation errors for the fields that are touched
   const formikInstance = useFormik({
     initialValues: {
       username: '',
@@ -40,8 +42,9 @@ const Form = (props) => {
             name='username'
             onChange={formikInstance.handleChange}
             value={formikInstance.values.username}
+            onBlur={formikInstance.handleBlur}
           />
-          {formikInstance.errors.username ? (
+          {formikInstance.touched.username && formikInstance.errors.username ? (
             <p className='text-danger'>Username is required</p>
           ) : null}
         </div>
@@ -54,8 +57,9 @@ const Form = (props) => {
             name='password'
             onChange={formikInstance.handleChange}
             value={formikInstance.values.password}
+            onBlur={formikInstance.handleBlur}
           />
-          {formikInstance.errors.password ? (
+          {formikInstance.touched.password && formikInstance.errors.password ? (
             <p className='text-danger'>Password is required</p>
           ) : null}
         </div>
