22b8c8e9d4c3f3b69711cf0decf98ad93af5febc
vivekanandpv <vivekanandpv@outlook.com>
2021-09-13 10:08:31 +0530

    done

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -3,8 +3,16 @@ import React from 'react';
 import { Fragment } from 'react/cjs/react.production.min';
 
 const Form = (props) => {
-  //  This instance manages form state, form submission, and validation errors
-  const formikInstance = useFormik({});
+  //  make sure the input controls have name attributes corresponding
+  //  to the properties of initialValues
+
+  //  Then, use the formikInstance to bind to onChange and value props
+  const formikInstance = useFormik({
+    initialValues: {
+      username: '',
+      password: '',
+    },
+  });
   return (
     <Fragment>
       <h3>Login Form</h3>
@@ -12,12 +20,24 @@ const Form = (props) => {
       <form>
         <div className='form-group'>
           <label htmlFor='username'>Username</label>
-          <input type='text' className='form-control' />
+          <input
+            type='text'
+            className='form-control'
+            name='username'
+            onChange={formikInstance.handleChange}
+            value={formikInstance.values.username}
+          />
         </div>
 
         <div className='form-group'>
           <label htmlFor='password'>Password</label>
-          <input type='password' className='form-control' />
+          <input
+            type='password'
+            className='form-control'
+            name='password'
+            onChange={formikInstance.handleChange}
+            value={formikInstance.values.password}
+          />
         </div>
 
         <button className='btn btn-primary'>Submit</button>
